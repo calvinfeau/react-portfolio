df55d8f8623e13482f2ec4b25b268f7dbc86048c
Calvin Féau <calvin@calvins-mbp.lan>
2019-10-16 12:03:11 -0700

    Updates

---
+++ precache-manifest.a644d6f4be7ebc82b869ec599fc80be2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dd30d81f9bd2d00376d41f0c3dac255",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1d52dff8dc3d363e5ff",
+    "url": "/static/css/main.93da19b5.chunk.css"
+  },
+  {
+    "revision": "694b60d59d3efb68b68b",
+    "url": "/static/js/2.fb84d1dd.chunk.js"
+  },
+  {
+    "revision": "c1d52dff8dc3d363e5ff",
+    "url": "/static/js/main.677b82f4.chunk.js"
+  },
+  {
+    "revision": "dd6d83088f6390651648",
+    "url": "/static/js/runtime-main.dfbddb88.js"
+  },
+  {
+    "revision": "c43a2890bf84ec7d246d291b42aced5e",
+    "url": "/static/media/EuclidFlexBold.c43a2890.woff"
+  },
+  {
+    "revision": "5977c2da335a44f0e11e6bd559e80c84",
+    "url": "/static/media/GT-Pressura-Mono-Regular.5977c2da.woff"
+  },
+  {
+    "revision": "869eb9e5904708d2b3f3109319f72194",
+    "url": "/static/media/gt-america-standard-regular.869eb9e5.woff"
+  }
+]);
